01d958d125ab156aae9982a1bc0657483db12e4f
Stanley Gehman <Stanley@HersheysMM.com>
2022-06-24 12:28:38 -0400

    Add files from session with Curtis

---
+++ Curtis.Auker.Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Kantaiko.ConsoleFormatting;
+
+Console.WriteLine("Hello, and welcome to our simple program demonstration!");
+var firstName = Prompt("Please type your first name");
+var lastName = Prompt("Please type your last name");
+Console.WriteLine($"Why hello {firstName} {lastName}! Welcome to my programming workshop.\nThis is a blink test: ");
+
+//Console.WriteLine(Decorations.Decoration("Blink", TextDecoration.Blink));
+Console.WriteLine(Decorations.Italic("Custom Text").FgRed());
+
+string? Prompt(string promptText)
+{
+    Console.WriteLine($"{promptText}: ");
+    var userInput = Console.ReadLine();
+    var allCaps = userInput?.ToUpper();
+    return allCaps;
+}
